b25c46397687e4d32c31b2e16c12922597715050
Kieran Noble <kieran@aylo.net>
2021-04-28 15:45:26 +0100

    Basic redirection Structure

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Redirections;
+use App\Models\RedirectionStatistics;
+
+class PageController extends Controller
+{
+    public function serve($path)
+    {
+        $redirect = Redirections::where('route', $path)->first();
+        if($redirect) {
+            $redirectionStatistic = RedirectionStatistics::firstOrNew(['id' => $redirect->id]);
+            $redirectionStatistic->usage = $redirectionStatistic->usage + 1;
+            $redirectionStatistic->save();
+
+            dd($redirect->statistics);
+
+            return redirect($redirect->url);
+        }
+        abort(404);
+    }
+}
