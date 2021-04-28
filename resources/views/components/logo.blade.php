f9365d112c5cd39369071d4d8e0e86dbbbbc98da
Kieran Noble <kieran@aylo.net>
2021-04-28 19:09:42 +0100

    automatic pride logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,7 @@
 @props(['class'])
 
-<img {{ $attributes->merge(['class' => $class]) }} src="{{ asset('aylopride.svg') }}"></img>
+@if(now()->month == 6)
+    <img {{ $attributes->merge(['class' => $class]) }} src="{{ asset('aylopride.svg') }}"></img>
+@else
+    <img {{ $attributes->merge(['class' => $class]) }} src="{{ asset('aylo.svg') }}"></img>
+@endif
