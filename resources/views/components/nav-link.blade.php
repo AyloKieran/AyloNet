95647c95a7f803a52e39c99166f784fd37bba85f
Kieran Noble <kieran@aylo.net>
2021-05-02 17:21:50 +0100

    nav update

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -4,14 +4,14 @@
 
 $classes = ($active ?? false)
             ? 'mb-3 pl-4 text-2xl font-bold'
-            : 'mb-3 pl-4 text-lg text-gray-300';
+            : 'mb-3 pl-4 text-lg text-gray-300 hover:text-gray-100 transition';
 $icon = ($icon ?? null)
             ? $icon
             : '';
 @endphp
 
 <div {{ $attributes->merge(['class' => $classes]) }}>
-    <a class="w-full inline-block" {{ $attributes->merge(['href' => $href]) }}>
+    <a class="w-full inline-block" href="{{$href}}">
             <span class="inline-block align-middle">{{ $slot }}</span>
     </a>
 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -82,19 +82,19 @@
         <x-nav-link :href="'/portfolio'" :active="request()->path() == 'portfolio'">
             Portfolio
         </x-nav-link>
-        <x-nav-link :href="'github'">
+        <x-nav-link :href="'/github'">
             Github
         </x-nav-link>
-        <x-nav-link :href="'contact'" :active="request()->path() == 'contact'">
+        <x-nav-link :href="'/contact'" :active="request()->path() == 'contact'">
             Contact
         </x-nav-link>
     </div>
 
-    
-        <div class="bg-gray-900 shadow-xl flex flex-row p-2">
+
+    <div class="bg-gray-900 flex flex-row p-2">
         @auth
             @if(!auth()->user()->avatar)
-                <svg class="h-8 w-8 p-1 fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg class="h-10 w-10 p-1 fill-current text-gray-400 rounded-full bg-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
             @else
