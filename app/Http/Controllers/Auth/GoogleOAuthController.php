3401b305d55a103278d7cc52db899a1d7a86a31e
Kieran Noble <kieran@aylo.net>
2021-05-02 19:20:27 +0100

    Fix Socialite?

--- app/Http/Controllers/Auth/GoogleOAuthController.php
+++ app/Http/Controllers/Auth/GoogleOAuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Laravel\Socialite\Facades\Socialite;
+use Socialite;
 use App\Models\User;
 use Carbon\Carbon;
 use Auth;
