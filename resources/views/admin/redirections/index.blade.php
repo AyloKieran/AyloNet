b3d85bde47d36e8bae21b0d15bc64f8210140224
Kieran Noble <kieran@aylo.net>
2021-05-08 14:57:36 +0100

    Fix image alt text

--- resources/views/admin/redirections/index.blade.php
+++ resources/views/admin/redirections/index.blade.php
@@ -53,7 +53,7 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                             </svg>
                                         @else
-                                            <img class="h-10 w-10 rounded-full" src="{{ $redirection->creator()->avatar }}" alt="{{ $redirection->creator()->first()->name }}'s Avatar" title="{{ $redirection->creator()->first()->name }}"></img>
+                                            <img class="h-10 w-10 rounded-full" src="{{ $redirection->creator()->avatar }}" alt="{{ $redirection->creator()->name }}'s Avatar" title="{{ $redirection->creator()->name }}"></img>
                                         @endif
                                         <div class="flex-grow lg:flex-grow-0"></div>
                                     </div>
