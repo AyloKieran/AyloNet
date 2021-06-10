12402aac14b4a14eef613a3cf19a4ccbdc353179
Kieran Noble <kieran@aylo.net>
2021-06-10 16:49:25 +0100

    PWA Install Assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <!-- PWA Assets -->
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/pwaAssets/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/pwaAssets/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/pwaAssets/favicon-16x16.png">
+        <link rel="manifest" href="/pwaAssets/site.webmanifest">
+        <link rel="mask-icon" href="/pwaAssets/safari-pinned-tab.svg" color="#3b4abc">
+        <link rel="shortcut icon" href="/pwaAssets/favicon.ico">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-config" content="/pwaAssets/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
+
         <title>{{ $title ?? '' }} - {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
