9070a59d2215c0ceceda3c91a3d1ff6cd6fa08e5
Kieran Noble <kieran@aylo.net>
2021-07-21 20:19:27 +0100

    add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+git stash --include-untracked
+git reset --hard
+git clean -fd
+git pull 
+
+composer install -n --optimize-autoloader --no-dev
+php artisan migrate
+npm run prod
+
+php artisan cache:clear
+php artisan view:clear
+php artisan config:clear
+php artisan route:clear
