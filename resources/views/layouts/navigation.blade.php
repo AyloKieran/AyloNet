95ae0a0b6ac3e72ae26fc38f5a7b6aa6d1bcfa3d
Kieran Noble <kieran@aylo.net>
2021-07-31 11:58:57 +0100

    fix read more link & avatar outline

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,7 +52,7 @@
                             d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                     </svg>
                     @else
-                    <img class="h-10 w-10 rounded-full object-cover" src="{{ auth()->user()->avatar }}"></img>
+                    <img class="h-10 w-10 rounded-full object-cover" src="{{ auth()->user()->avatar }}" />
                     @endif
                 </div>
 
@@ -126,13 +126,13 @@
         <a href="{{ route('user') }}" class="flex flex-row">
             <div class="w-10">
                 @if(!auth()->user()->avatar)
-                <svg class="h-10 w-10 p-1 fill-current text-gray-400 rounded-full bg-gray-800"
+                <svg class="h-10 w-10 p-1 fill-current text-gray-400 rounded-full bg-gray-800 border-solid border-2 border-gray-700 hover:border-gray-500 transition"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
                 @else
-                <img class="h-10 w-10 rounded-full object-cover" src="{{ auth()->user()->avatar }}"></img>
+                <img class="h-10 w-10 rounded-full object-cover border-solid border-2 border-gray-800 hover:border-gray-500 transition" src="{{ auth()->user()->avatar }}"></img>
                 @endif
             </div>
 
